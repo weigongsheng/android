b4974cc265796e570df55ea1b79fcf846c3446f4
weigongsheng <weigongsheng126@126.com>
2015-08-15 15:15:14 +0800

    ddd

---
+++ baselib/src/main/java/com/hiuzhong/baselib/util/AndroidVersionBean.java
@@ -0,0 +1,34 @@
+package com.hiuzhong.baselib.util;
+
+/**
+ * Created by gongsheng on 2015/8/15.
+ */
+public class AndroidVersionBean {
+    private String url;
+    private String versionName;
+    private String versionCode;
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getVersionName() {
+        return versionName;
+    }
+
+    public void setVersionName(String versionName) {
+        this.versionName = versionName;
+    }
+
+    public void setVersionCode(String versionCode) {
+        this.versionCode = versionCode;
+    }
+
+    public String getVersionCode() {
+        return versionCode;
+    }
+}
