ba3c9e3ce9fad228d5c2170c722e7b9398984797
weigongsheng <weigongsheng126@126.com>
2015-08-15 15:13:39 +0800

    ddd

--- yuxun/src/main/java/com/hiuzhong/yuxun/RequireBdGrantActivity.java
+++ yuxun/src/main/java/com/hiuzhong/yuxun/RequireBdGrantActivity.java
@@ -3,11 +3,16 @@ package com.hiuzhong.yuxun;
 import android.app.Activity;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.text.Layout;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.ScrollView;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -35,7 +40,9 @@ public class RequireBdGrantActivity extends Activity {
 
     ContactsDbManager contactDao;
 
-    private View[] grantViewList = new View[3];
+//    private View[] grantViewList = new View[3];
+    private LinearLayout msgContainer;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -59,9 +66,8 @@ public class RequireBdGrantActivity extends Activity {
         }).callWs(myAccount.optString("account"), myAccount.optString("pwd"));
         cancelClient = WebServiceHelper.createGrantCancelClient(this, new WsCallBack() {
             @Override
-            public void whenResponse(JSONObject json,Object ... extra) {
-                grantViewList[(Integer) extra[0]].setVisibility(View.GONE);
-                contactDao.deleteContact((String)extra[1]);
+            public void whenResponse(JSONObject json, Object... extra) {
+                msgContainer.removeView((View) extra[0]);
             }
         });
         requestClient = WebServiceHelper.createGrantRegClient(this, new WsCallBack() {
@@ -70,37 +76,36 @@ public class RequireBdGrantActivity extends Activity {
                 Toast.makeText(RequireBdGrantActivity.this, json.optString("tip"), Toast.LENGTH_LONG).show();
             }
         });
-        grantViewList[0] = findViewById(R.id.layoutCancel1);
-        grantViewList[1] = findViewById(R.id.layoutCalcel2);
-        grantViewList[2] = findViewById(R.id.layoutCalcel3);
         ActivityHelper.initHeadInf(this);
+        msgContainer = (LinearLayout) findViewById(R.id.msgScrollContainer);
     }
 
     protected void showGrant(JSONArray jsonArray) {
         if(jsonArray == null || jsonArray.length()<1){
             return;
         }
-        grantBdInfo = jsonArray;
-        initGrantView( grantViewList[0],R.id.tipBdNum1,R.id.cancelGran1,jsonArray.optJSONObject(0),0);
-        initGrantView( grantViewList[1],R.id.tipBdNum2,R.id.cancelGran2,jsonArray.optJSONObject(1),1);
-        initGrantView( grantViewList[2],R.id.tipBdNum3,R.id.cancelGran3,jsonArray.optJSONObject(2),2);
-
-
-    }
-
-    private void initGrantView(View view, int tipBdNum1, int cancelGran1, JSONObject json,int index) {
-        if(json == null){
-            return;
+//        grantBdInfo = jsonArray;
+//        initGrantView( grantViewList[0],R.id.tipBdNum1,R.id.cancelGran1,jsonArray.optJSONObject(0),0);
+//        initGrantView( grantViewList[1],R.id.tipBdNum2,R.id.cancelGran2,jsonArray.optJSONObject(1),1);
+//        initGrantView(grantViewList[2], R.id.tipBdNum3, R.id.cancelGran3, jsonArray.optJSONObject(2), 2);
+        for (int i = 0,j=jsonArray.length(); i <j ; i++) {
+            inflateReceivedMsg(jsonArray.optJSONObject(i).optString("BdNum"));
         }
-        view.setVisibility(View.VISIBLE);
-        ((TextView)view.findViewById(tipBdNum1)).setText("北斗号:" + json.optString("BdNum"));
-        ((Button)view.findViewById(cancelGran1)).setTag(index);
     }
+
+//    private void initGrantView(View view, int tipBdNum1, int cancelGran1, JSONObject json,int index) {
+//        if(json == null){
+//            return;
+//        }
+//        view.setVisibility(View.VISIBLE);
+//        ((TextView)view.findViewById(tipBdNum1)).setText("北斗号:" + json.optString("BdNum"));
+//        ((Button)view.findViewById(cancelGran1)).setTag(index);
+//    }
 
 
     public void cancelGrant(View v){
-//        curIndex = (int) v.getTag();
-        cancelClient.callWs(myAccount.optString("account"), myAccount.optString("pwd"), grantBdInfo.optJSONObject(curIndex).optString("BdNum"),(int) v.getTag(), 0 );
+//        msgContainer.removeView((View) v.getParent());
+        cancelClient.callWs(myAccount.optString("account"), myAccount.optString("pwd"), v.getTag().toString(), v.getParent());
          //todo
     }
 
@@ -112,6 +117,13 @@ public class RequireBdGrantActivity extends Activity {
         requestClient.callWs(myAccount.optString("account"), myAccount.optString("pwd"),reqNum);
     }
 
+    private void inflateReceivedMsg(String bdNum){
+        View sentView = LayoutInflater.from(this).inflate(R.layout.layout_grant, null);
+        ((TextView) sentView.findViewById(R.id.tipBdNum)).setText("北斗号:" + bdNum);
+        sentView.findViewById(R.id.cancelGran).setTag(bdNum);
+        msgContainer.addView(sentView);
+    }
+    
 
 
 }
